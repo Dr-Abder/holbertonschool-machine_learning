7d54ad8fe85f6f9c8eccace602bc1d4acc56bc44
Dr-Abder <9907@holbertonstudents.com>
2025-08-07 15:13:31 +0000

    push de la task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,26 +5,5 @@ Cette fonction sert à transposer une matrice
 
 
 def matrix_transpose(matrix):
-    """
-    transpose de matrix
-    """
-    rows = len(matrix)
-    """
-    pour les lignes
-    """
-    cols = len(matrix[0])
-    """
-    pour les collones
-    """
 
-    transposed_matrix = [[0 for index in range(rows)] for index in range(cols)]
-    """
-    créations de la matrice qui sera transposer
-    """
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-            """
-            copie et transpose de la matrice en inversant les index i et j
-            """
-    return transposed_matrix
+    return [list(row) for row in zip(*matrix)]
