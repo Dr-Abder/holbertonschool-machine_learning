b3149e58076c323bdc37c47079ff66e942068a03
Dr-Abder <9907@holbertonstudents.com>
2025-08-07 15:21:46 +0000

    push

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """
-Cette fonction sert à transposer une matrice
+Cette fonction sert à transposer une matrice 2D
 """
 
 
 def matrix_transpose(matrix):
-
+    """
+    transpose de matrix
+    """
     return [list(row) for row in zip(*matrix)]
