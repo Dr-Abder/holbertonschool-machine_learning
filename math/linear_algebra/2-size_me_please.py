faa5f68927d83adc0e1363f98577ba941c90cf8f
Dr-Abder <9907@holbertonstudents.com>
2025-08-07 13:50:52 +0000

    commit de la task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-def matrix_shape(matrix):   #récursivité
+def matrix_shape(matrix):
+    """récursivité"""
     if not isinstance(matrix, list):
-        return []   #si matrix n'est pas une liste
+        return []
+        """si matrix n'est pas une liste"""
     if isinstance(matrix[0], list):
-        return [len(matrix)] + matrix_shape(matrix[0]) #pour une matrice 3D (ou plus)
+        return [len(matrix)] + matrix_shape(matrix[0])
+        """pour une matrice 3D (ou plus)"""
     else:
-        return [len(matrix)] #pour une matrice 2D
+        return [len(matrix)]
+        """pour une matrice 2D"""
