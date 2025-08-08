87c7305ddf9458c62d834e9e8b4f8d33b375e694
Dr-Abder <9907@holbertonstudents.com>
2025-08-08 09:27:48 +0000

    push task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 """
-
+Cette fonction additionne les tableaus élément par élément
 """
 
 
 def add_arrays(arr1, arr2):
     """
-    
+    arr1 + arr2
     """
-    
+
+    if len(arr1) != len(arr2):
+        return None
+    """
+    vérification des tailles
+    """
+    arr3 = [a + b for a, b in zip(arr1, arr2)]
+    """
+    créations de paire a et b pour chaque array et on les additiones (a + b)
+    et cela position par position
+    """
+    return arr3
