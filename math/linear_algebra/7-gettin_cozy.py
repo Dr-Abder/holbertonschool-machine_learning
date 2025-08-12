b020d62cb98e9ca18414ea6372ad2532f04e81fa
Dr-Abder <9907@holbertonstudents.com>
2025-08-12 17:33:18 +0000

    push task7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,7 +6,7 @@ Cette fonction concatène mat1 et mat2 qui sont des matrices
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """
-        concatène mat1 et mat2
+    concatène mat1 et mat2
     """
 
     if axis == 0:
@@ -15,7 +15,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
         else:
             return None
         """
-        blablba
+        Vérifie sur l'axe 0 (concaténation verticale, empilement de lignes)
+        Si mat1 et mat2 ont le même nombre de colonnes,
+        alors on les concatène en empilant mat2 sous mat1 (mat1 + mat2),
+        sinon on retourne None.
         """
 
     if axis == 1:
@@ -24,5 +27,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
         else:
             return None
         """
-        bloblo
+        Vérifie sur l'axe 1 (concaténation horizontale, extension des colonnes)
+        Si mat1 et mat2 ont le même nombre de lignes,
+        alors on concatène chaque ligne de mat1 avec la ligne correspondante de mat2 
+        grace à la boucle qui passe de ligne en ligne pour chaque matrice ,
+        sinon on retourne None.
         """
