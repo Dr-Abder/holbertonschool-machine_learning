6cb2d03c88ec4a3d9f9b9c659f1bfaa5aa7d98d0
Dr-Abder <9907@holbertonstudents.com>
2025-08-14 14:57:50 +0000

    push task0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
 """
 Fonction qui trace une courbe y = x³ graphique linéaire
 """
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 def line():
@@ -13,17 +13,13 @@ def line():
 
     y = np.arange(0, 11) ** 3
     # Crée un tableau de 0 à 10 au cube (valeur de l'axe y)
-
     plt.figure(figsize=(6.4, 4.8))
     # Crée la nouvelle figure (zone graphique)
-
-    plt.xlim(0, 10)
+    x = np.arange(0, 10)
     # Crée un tableau de 0 à 10 (valeur de l'axe x)
-
-    plt.plot(y, 'r-')
+    plt.plot(x, y, 'r-')
     # Trace les points x[i], y[i] (Chaque valeur x[i] correspond à y[i])
     # 'r-' = ligne rouge continue
 
     plt.savefig("0-line.png")
     plt.show()
-    # Enregistre le graphique et le montre
