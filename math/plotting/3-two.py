fb54bc42401bec920b89a639c59aec7f84d6022e
Dr-Abder <9907@holbertonstudents.com>
2025-08-15 03:21:52 +0000

    push task3

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -19,14 +19,14 @@ def two():
     plt.figure(figsize=(6.4, 4.8))
 
     # Spécification du graphique (couleur, titre, échelle, limite)
-    plt.plot(x, y1, 'r--')
-    plt.plot(x, y2, 'g-')
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
     plt.title("Exponential Decay of C-14")
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
-    plt.legend(["C-14", "Ra-226"], loc="upper right")
+    plt.legend(loc="upper right")
 
     plt.savefig("3-two.png")
     plt.show()
