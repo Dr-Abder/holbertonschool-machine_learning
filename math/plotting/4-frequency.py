f3137752daac462a27efb703e77535397c5aeb73
Dr-Abder <9907@holbertonstudents.com>
2025-08-15 03:49:10 +0000

    push task4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+graphique a deux courbe
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Fonction qui retoure deux courbe
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+# Spécification du graphique (couleur, titre, échelle, limite)
+    bins = np.arange(0, 101, 10)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.hist(student_grades, bins, edgecolor='black')
+    plt.xticks(np.arange(0, 110, 10))
+    plt.savefig("4-frequency.png")
+    plt.show()
