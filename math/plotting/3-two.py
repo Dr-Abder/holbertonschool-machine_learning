24d3a57564a097e32a766371008f442f8c9db3a7
Dr-Abder <9907@holbertonstudents.com>
2025-08-15 03:18:43 +0000

    push task3

---
+++ math/plotting/3-two.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+Fonction qui retoure deux courbe
+"""
+
+
+def two():
+    """
+    graphique a deux courbe
+    """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Spécification du graphique (couleur, titre, échelle, limite)
+    plt.plot(x, y1, 'r--')
+    plt.plot(x, y2, 'g-')
+    plt.title("Exponential Decay of C-14")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend(["C-14", "Ra-226"], loc="upper right")
+
+    plt.savefig("3-two.png")
+    plt.show()
