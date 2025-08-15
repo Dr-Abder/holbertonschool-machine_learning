0b909969426892d07b456cbe508066230c9ae75e
Dr-Abder <9907@holbertonstudents.com>
2025-08-15 03:50:13 +0000

    push task4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,10 +14,11 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-# Spécification du graphique (couleur, titre, échelle, limite)
+    # Spécification du graphique (couleur, titre, échelle, limite)
     bins = np.arange(0, 101, 10)
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
+    plt.title('Project A')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
     plt.hist(student_grades, bins, edgecolor='black')
