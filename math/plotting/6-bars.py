3facc419d58f13458b8600db7d2b0a039b449c8e
Dr-Abder <9907@holbertonstudents.com>
2025-08-17 01:50:57 +0000

    push task6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,8 +2,8 @@
 """
 This function generates a stacked bar graph
 """
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def bars():
@@ -11,26 +11,27 @@ def bars():
     Requires matplotlib and numpy libraries
     """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     # Spécification du graphique (couleur, titre, échelle, limite)
     personne = ['Farrah', 'Fred', 'Felicia']
-    couleur = ['red', 'yellow', '#ff8000','#ffe5b4']
+    couleur = ['red', 'yellow', '#ff8000', '#ffe5b4']
     fruit_labels = ['apples', 'bananas', 'oranges', 'peaches']
 
     barres = np.arange(3)
 
-    # Spécification du graphique (couleur, titre, échelle, limite)
-    plt.bar(barres, fruit[0], color=couleur[0], width=0.5)
-    plt.bar(barres, fruit[1], color=couleur[1], width=0.5, bottom=fruit[0])
-    plt.bar(barres, fruit[2], color=couleur[2], width=0.5, bottom=fruit[0]+fruit[1])
-    plt.bar(barres, fruit[3], color=couleur[3], width=0.5, bottom=fruit[0]+fruit[1]+fruit[2])
-    plt.xticks(barres, personne)
-    plt.ylabel("Quantity of Fruit")
+    # Create stacked bars
+    for idx, row in enumerate(fruit):
+        plt.bar(people, row, color=colors[idx], label=fruits[idx],
+                bottom=bottom, width=0.5)
+        bottom += row
+
+    # Labeling and setting limits
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))
-    plt.title("Number of Fruit per Person")
-    plt.legend(fruit_labels)
+    plt.title('Number of Fruit per Person')
 
     # Show the plot
     plt.savefig("6-bars.png")
