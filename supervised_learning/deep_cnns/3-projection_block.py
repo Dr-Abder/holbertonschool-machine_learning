dc08a016d8b5e32a346f0b8944ae0e1a106e612b
Dr-Abder <9907@holbertonstudents.com>
2025-10-13 19:02:21 +0000

    3-projection_block.py

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -6,11 +6,15 @@ un réseau résiduel (ResNet) utilisant TensorFlow et Keras.
 Ce bloc est une variante du bloc d'identité utilisée lorsque les
 dimensions du tenseur d'entrée et de sortie diffèrent. Il introduit
 une projection par convolution 1x1 dans le chemin de raccourci
-(shortcut) afin d'adapter la taille et le nombre de canaux avant
-l'addition résiduelle.
+(shortcut) afin d'adapter la taille et le nombre de canaux
+avant l'addition résiduelle.
 """
 
 from tensorflow import keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+
     """
     Construit un bloc de projection pour un réseau résiduel (ResNet).
 
@@ -40,29 +44,26 @@ from tensorflow import keras as K
         >> print(output.shape)
     """
 
-def projection_block(A_prev, filters, s=2):
-
-
     F11, F3, F12 = filters
     init = K.initializers.HeNormal(seed=0)
 
     conv1 = K.layers.Conv2D(F11, (1, 1), strides=(s, s),
-                    padding='same', kernel_initializer=init)(A_prev)
+                            padding='same', kernel_initializer=init)(A_prev)
     norm1 = K.layers.BatchNormalization(axis=3)(conv1)
     relu1 = K.layers.Activation("relu")(norm1)
 
     conv2 = K.layers.Conv2D(F3, (3, 3), (1, 1),
-                    padding='same', kernel_initializer=init)(relu1)
+                            padding='same', kernel_initializer=init)(relu1)
     norm2 = K.layers.BatchNormalization(axis=3)(conv2)
     relu2 = K.layers.Activation("relu")(norm2)
 
     conv3 = K.layers.Conv2D(F12, (1, 1), (1, 1),
-                    padding='same', kernel_initializer=init)(relu2)
+                            padding='same', kernel_initializer=init)(relu2)
     norm3 = K.layers.BatchNormalization(axis=3)(conv3)
 
-
     conv_shortcut = K.layers.Conv2D(F12, (1, 1), strides=(s, s),
-                    padding='same', kernel_initializer=init)(A_prev)
+                                    padding='same',
+                                    kernel_initializer=init)(A_prev)
     norm_shortcut = K.layers.BatchNormalization(axis=3)(conv_shortcut)
     shortcut = K.layers.Add()([norm3, norm_shortcut])
 
