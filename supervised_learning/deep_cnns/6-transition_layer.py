729c01ac7563a84166185bc18ff864c4eda771d3
Dr-Abder <9907@holbertonstudents.com>
2025-10-14 19:30:17 +0000

    6-transition_layer.py

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -6,5 +6,14 @@ from tensorflow import keras as K
 
 def transition_layer(X, nb_filters, compression):
 
+    init = K.initializers.HeNormal(seed=0)
 
-    return 
+    new_nbf = int(nb_filters * compression)
+
+    BN1 = K.layers.BatchNormalization(axis=3)(X)
+    RL1 = K.layers.Activation('relu')(BN1)
+    CNV1 = K.layers.Conv2D(new_nbf, (1,1), padding='same',
+                           kernel_initializer=init)(RL1)
+    AVG = K.layers.AveragePooling2D((2, 2), padding='same',)(CNV1)
+
+    return AVG, new_nbf
