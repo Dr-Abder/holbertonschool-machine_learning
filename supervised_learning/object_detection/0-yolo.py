0cd80ef9e8a6fc4202eee289580c638a9d504a50
Dr-Abder <9907@holbertonstudents.com>
2025-10-22 13:13:53 +0000

    0-yolo.py

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from tensorflow import keras as K
+
+class Yolo:
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, "r") as f:
+            self.class_names = f.read().strip().split('\n')
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
