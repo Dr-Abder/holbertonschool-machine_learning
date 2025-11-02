1cc78cee266de26510574d163b4b549419d84ba7
Dr-Abder <9907@holbertonstudents.com>
2025-11-02 20:11:15 +0000

    all task

--- supervised_learning/object_detection/1-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from tensorflow import keras as K
-
-inception_network = __import__('1-inception_network').inception_network
-
-if __name__ == '__main__':
-    model = inception_network()
-    model.summary()
