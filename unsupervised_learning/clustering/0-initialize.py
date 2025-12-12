8876ddad942dda7897ac6aae8598b4517d747bb3
Dr-Abder <9907@holbertonstudents.com>
2025-12-12 16:45:22 +0000

    refont clustering

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -8,28 +8,19 @@ import numpy as np
 
 
 def initialize(X, k):
-    """
-    Initializes cluster centroids for K-means clustering.
 
-    Parameters:
-    X (numpy.ndarray): A 2D numpy array of shape (n, d) containing the dataset
-                       that will be used for K-means clustering.
-                       - n is the number of data points
-                       - d is the number of dimensions for each data point
-    k (int): A positive integer representing the number of clusters.
-
-    Returns:
-    numpy.ndarray: A 2D numpy array of shape (k, d) containing the initialized
-                   centroids for each cluster.
-                   Returns None on failure
-    """
-
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(X, np.ndarray):
         return None
     if not isinstance(k, int) or k <= 0:
         return None
+    if len(X.shape) != 2:
+        return None
 
-    low_values = np.min(X, axis=0)
-    high_values = np.max(X, axis=0)
+    n, d = X.shape
 
-    return np.random.uniform(low_values, high_values, size=(k, X.shape[1]))
+    mins = np.min(X, axis=0)
+    maxs = np.max(X, axis=0)
+
+    centroids = np.random.uniform(mins, maxs, size=(k, d))
+
+    return centroids
